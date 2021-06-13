3c5913b6249d93f593b5c8e95e7ec26a7634430a
maru <nightlerben3@hotmail.com>
2021-06-13 22:16:40 +0900

    トリボナッチ数列への拡張2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const memo = new Map();
 memo.set(0, 0);
 memo.set(1, 0);
-memo.set(2, 1)
+memo.set(2, 1);
 function trib(n) {
     if (memo.has(n)) {
         return memo.get(n);
@@ -11,7 +11,7 @@ function trib(n) {
     memo.set(n, value);
     return value;
 }
-const length = 100;
+const length = 40;
 for (let i = 0; i <= length; i++) {
     console.log(trib(i));
 }
