76d34a4c70e2364285e03364729f714fe8a4b509
kinoa kanawashi <kanawashi@kinoanoAir.elecom>
2021-06-15 14:43:50 +0900

    第２０項までの簡易テスト実装

--- app.js
+++ app.js
@@ -11,6 +11,17 @@ function trib(n) {
     memo.set(n, value);
     return value;
 }
+
+const fact = [0, 0, 1, 1, 2, 4, 7, 13, 24, 44, 81, 149, 274, 504, 927, 1705, 3136, 5768, 10609, 19513, 35890];
+const assert = require('assert');
+for (let i = 0; i < fact.length; i++){
+    assert.strictEqual(
+        trib(i), 
+        fact[i],
+        `この数列の第\i項は${fact[i]}ですが、実際は${trib(i)}でした`);
+}
+console.log('第20項までの簡易テストを通過しました');
+
 const length = 40;
 for (let i = 0; i <= length; i++) {
     console.log(trib(i));
