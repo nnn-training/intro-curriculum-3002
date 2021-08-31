320e91a903dd186d927d65f030b5f1d457576721
nakako <nakako.soft@gmail.com>
2021-08-31 20:30:36 +0900

    トリボナッチ数列を出力するコードに変更

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 'use strict';
+const length = process.argv[2] || 0;
 const memo = new Map();
 memo.set(0, 0);
-memo.set(1, 1);
+memo.set(1, 0);
+memo.set(2, 1);
 function trib(n) {
-    if (memo.has(n)) {
-        return memo.get(n);
+    if (memo.has(n) === false) {
+        // 未計算のため、計算してmemoにセットする
+        memo.set(n, memo.get(n-1) + memo.get(n-2) + memo.get(n-3));
     }
-    const value = trib(n - 1) + trib(n - 2);
-    memo.set(n, value);
-    return value;
+    return memo.get(n);
+
 }
-const length = 40;
+
 for (let i = 0; i <= length; i++) {
     console.log(trib(i));
 }
