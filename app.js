3dd8fb039cf8621f6d1611ce0aff4738f8ac7785
Happy Kumamoto <zen.word+hub@gmail.com>
2021-09-09 00:46:47 +0900

    ペンタナッチを実装

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ const memo = new Map();
 memo.set(0, 0);
 memo.set(1, 0);
 memo.set(2, 0);
-memo.set(3, 1);
-function tetra(n) {
+memo.set(3, 0);
+memo.set(4, 1);
+function penta(n) {
   if (memo.has(n)) {
     return memo.get(n);
   }
-  const value = tetra(n - 1) + tetra(n - 2)+ tetra(n - 3)+ tetra(n - 4);
+  const value = penta(n - 1) + penta(n - 2)+ penta(n - 3)+ penta(n - 4)+ penta(n - 5);
   memo.set(n, value);
   return value;
 }
 const length = 40;
 for (let i = 0; i <= length; i++) {
-  console.log(tetra(i));
+  console.log(penta(i));
 }
