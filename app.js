e0cff4e257ff617b4f1aeb016ef744f62b5eb4e3
Happy Kumamoto <zen.word+hub@gmail.com>
2021-09-09 00:52:57 +0900

    ヘキサナッチを実装

--- app.js
+++ app.js
@@ -4,16 +4,18 @@ memo.set(0, 0);
 memo.set(1, 0);
 memo.set(2, 0);
 memo.set(3, 0);
-memo.set(4, 1);
-function penta(n) {
+memo.set(4, 0);
+memo.set(5, 1);
+function hexa(n) {
   if (memo.has(n)) {
     return memo.get(n);
   }
-  const value = penta(n - 1) + penta(n - 2)+ penta(n - 3)+ penta(n - 4)+ penta(n - 5);
+  const value = hexa(n - 1) + hexa(n - 2)+ hexa(n - 3)
+  + hexa(n - 4) + hexa(n - 5) + hexa(n - 6);
   memo.set(n, value);
   return value;
 }
 const length = 40;
 for (let i = 0; i <= length; i++) {
-  console.log(penta(i));
+  console.log(hexa(i));
 }
